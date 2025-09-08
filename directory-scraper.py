9dcb6625292b64dd7cae79f78257262fa4639606
Clarissa Fonseca Chen <clarissafchen@gmail.com>
2025-09-08 11:14:08 -0500

    Update directory-scraper.py
    scraper updated for wider use case

--- directory-scraper.py
+++ directory-scraper.py
@@ -1,21 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-def fetch_directory(url):
+def scrape_expert_directory(url):
     res = requests.get(url)
     res.raise_for_status()
-    soup = BeautifulSoup(res.text, "html.parser")
+    soup = BeautifulSoup(res.text, 'html.parser')
 
-    experts = {}
-    for block in soup.select("div.expert-block"):  # adjust selectors as needed
-        name_tag = block.select_one(".expert-name")
-        link_tags = block.select("a")
+    expert_data = {}
+    for expert in soup.select(".expert-item"):  # Adjust this selector based on actual HTML
+        name_tag = expert.select_one(".expert-name")
+        link_tags = expert.select("a")
+
         if name_tag:
             name = name_tag.get_text(strip=True)
-            urls = [a["href"] for a in link_tags if a.get("href")]
-            experts[name.lower()] = urls
-    return experts
-
-directory = fetch_directory("https://www.mlforseo.com/experts/")
-
-# Later, use `directory` when processing submitted URLs.
+            urls = [a["href"] for a in link_tags if a.get("href") and a["href"].startswith("http")]
+            expert_data[name.lower()] = urls
+    return expert_data
